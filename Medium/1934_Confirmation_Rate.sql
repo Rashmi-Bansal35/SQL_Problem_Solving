37460856d6667138df15b65cc5af72d9a6bc6501
Rashmi Bansal <gupta126idraghav@gmail.com>
2026-04-14 17:49:37 +0530

    Create 1934_Confirmation_Rate.sql

---
+++ Medium/1934_Confirmation_Rate.sql
@@ -0,0 +1,7 @@
+-- Problem: Confirmation Rate
+-- Platform: LeetCode
+-- Difficulty: Medium
+Select s.user_id, round(ifnull(avg(c.action = 'confirmed'), 0), 2) as confirmation_rate 
+from Signups s left join Confirmations c 
+on s.user_id = c.user_id 
+group by s.user_id;
