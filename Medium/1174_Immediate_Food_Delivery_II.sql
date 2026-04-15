0ae19506f481643284a1710418c16fb9c8e9500e
Rashmi Bansal <gupta126idraghav@gmail.com>
2026-04-15 21:42:20 +0530

    Create 1174_Immediate_Food_Delivery_II.sql

---
+++ Medium/1174_Immediate_Food_Delivery_II.sql
@@ -0,0 +1,10 @@
+-- Problem: Immediate Food Delivery II
+-- Platform: LeetCode
+-- Difficulty: Medium
+
+Select round(avg(order_date = customer_pref_delivery_date)*100,2) as immediate_percentage
+from Delivery 
+where (customer_id, order_date) 
+in (Select customer_id, min(order_date)
+    from delivery
+    group by customer_id);
