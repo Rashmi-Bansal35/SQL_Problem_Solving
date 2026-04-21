b36a6107a2d6bc4d46d83342a7cc809047b18d93
Rashmi Bansal <bansal35indrashmi@gmail.com>
2026-04-21 18:27:57 +0530

    Create 1204_Last_Person_to_Fit_in_the_Bus.sql

---
+++ Medium/1204_Last_Person_to_Fit_in_the_Bus.sql
@@ -0,0 +1,9 @@
+-- Problem: Last Person to Fit in the Bus
+-- Platform: LeetCode
+-- Difficulty: Medium
+
+Select q1.person_name from Queue q1
+join Queue q2 on q1.turn >= q2.turn
+group by q1.turn, q1.person_name
+having sum(q2.weight) <= 1000
+order by q1.turn desc limit 1;
