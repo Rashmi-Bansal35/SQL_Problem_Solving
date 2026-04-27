04cb58af91a1248ac7b0c472c3a5f652c9fb52cd
Rashmi Bansal <bansal35indrashmi@gmail.com>
2026-04-27 22:05:16 +0530

    Create 1141_User_Activity_for_the_Past_30_Days_I.sql

---
+++ Easy/1141_User_Activity_for_the_Past_30_Days_I.sql
@@ -0,0 +1,8 @@
+-- Problem: User Activity for the Past 30 Days I
+-- Platform: LeetCode
+-- Difficulty: Easy
+
+Select activity_date as day, count(distinct user_id) as active_users 
+from Activity 
+where activity_date between '2019-06-28' and '2019-07-27' 
+group by activity_date;
